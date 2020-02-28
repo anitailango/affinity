481d6465ae347dd9011581fa80128a80945ba326
Chingyi Ie <iechingyi@yahoo.com>
2020-02-28 14:49:27 -0800

    added python files

---
+++ lib/model.py
@@ -0,0 +1,25 @@
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+
+def setup_ensemble():
+    """
+    Returns an ensemble of 3 models (SVM, Naive Bayes, and Logistic Regression)
+    """
+
+    clf1 = SVC(probability=True)
+    clf2 = MultinomialNB()
+    clf3 = LogisticRegression()
+
+    estimators = [
+                  ('svm', clf1),
+                  ('naivebayes', clf2), 
+                  ('logistic', clf3)
+                  ]
+
+    ensemble = VotingClassifier(estimators, voting='soft')
+
+    return ensemble
+
+
