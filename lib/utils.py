ee55aeeef01d359abfce117b0bc439d7ae0c2521
Chingyi Ie <iechingyi@yahoo.com>
2020-03-01 14:34:35 -0800

    added lib functions

--- lib/utils.py
+++ lib/utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import MinMaxScaler
+import pickle
+
+from model import setup_ensemble
 
 def load_data():
     """
@@ -38,3 +41,33 @@ def preprocessing_pipeline(df):
     X = pd.DataFrame(scaled, columns=col_names)
 
     return X, y
+
+def train_model(data):
+    """
+    Input: data to train on with predictors and response
+    Output: trained model
+    """
+    # Preprocessing data
+    X, y = preprocessing_pipeline(data)
+
+    # Setting up model
+    clf = setup_ensemble()
+
+    # Fitting model
+    clf = clf.fit(X, y)
+
+    return clf
+
+def save_model(model, filename):
+    """
+    save the model to disk
+    """
+    pickle.dump(model, open(filename, 'wb'))
+
+def load_model(filename):
+    """
+    load the model from a disk
+    """
+    loaded_model = pickle.load(open(filename, 'rb'))
+    
+    return loaded_model
