be399ca3010a22a3f9f5553ce791c9c4656f7169
Edward Zhang <edwardz123@gmail.com>
2020-08-09 14:55:23 -0700

    Remove TopBar

--- chrome-extension-react/src/App.js
+++ chrome-extension-react/src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import TopBar from './components/TopBar.js';
-import ArticleInfo from './components/ArticleInfo.js';
-import Rating from './components/Rating.js';
+import React from "react";
+import ArticleInfo from "./components/ArticleInfo.js";
+import CircleButton from "./components/CircleButton";
+import questionIcon from "./assets/icons/icon-question.png";
+import logo from "./assets/icons/logoface-affinity-grey.png";
 
 function App() {
-  return (
-    <div className="App" style={styles}>
-      <TopBar/>
-      <ArticleInfo/>
-      {/* <Rating/> */}
-      
-    </div>
-  );
+	return (
+		<div className="App" style={containerStyle}>
+			<div class="pa3 flex justify-between" style={topBarStyle}>
+				<CircleButton icon={questionIcon} />
+				<img src={logo} style={logoStyle} className="tc pv2" alt="logo" />
+				<CircleButton icon={questionIcon} />
+			</div>
+			<ArticleInfo />
+		</div>
+	);
 }
 
-const styles = {
-  minWidth: "314px",
-  background: "#F8F8F8"
-}
+const containerStyle = {
+	minWidth: "314px",
+	background: "#F8F8F8",
+};
+
+const logoStyle = {
+	width: "50px",
+	height: "17px",
+};
+
+const topBarStyle = {
+	height: "52px",
+};
 
 export default App;
